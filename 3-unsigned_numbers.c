88c075caa9b99f0e8b09e46950057343cd25b41e
ChelimoBirir <Jelimobirir@gmail.com>
2023-03-27 11:04:56 -0700

    task 3

--- 3-unsigned_numbers.c
+++ 3-unsigned_numbers.c
@@ -71,7 +71,7 @@
  */int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	return (print_any_hexa(types, "0123456789abcdef", buffer,
+	return (print_upper_or_lower_hexa(types, "0123456789abcdef", buffer,
 		flags, 'x', width, precision, size));
 }
 
@@ -82,15 +82,15 @@
  */int print_upper_hexa(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	return (print_any_hexa(types, "0123456789ABCDEF", buffer,
+	return (print_upper_or_lower_hexa(types, "0123456789ABCDEF", buffer,
 		flags, 'X', width, precision, size));
 }
 
 /*
- * print_any_hexa - Prints a hexadecimal number in lower or upper
+ * print_upper_or_lower_hexa - Prints a hexadecimal number in lower or upper
  * 
  * Return: Int(number of characters printed)
- */int print_any_hexa(va_list types, char map_to[], char buffer[],
+ */int print_upper_or_lower_hexa(va_list types, char map_to[], char buffer[],
 int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
